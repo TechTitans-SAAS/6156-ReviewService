1ac83df5d4da2bd88aea7f0aa4332784fceada55
Meixuan Lu <meixvan.lu@mail.utoronto.ca>
2023-12-22 01:22:03 -0500

    added cors

--- Reviews/__init__.py
+++ Reviews/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 # app.config["SECRET_KEY"] = "somekey"
 app.config["MONGO_URI"] = "mongodb+srv://thriftUStore:dbUserPassword@cluster0.xzhgoim.mongodb.net/ThriftUStore?retryWrites=true&w=majority"
 
